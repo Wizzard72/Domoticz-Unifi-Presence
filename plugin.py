e47e777ead9b428258dc132921e246d19b91f004
Wizzard72 <paulslats@gmail.com>
2019-12-15 08:54:30 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -15,7 +15,8 @@
         <param field="Password" label="Password" width="600px" required="true" default="password" password="true"/>
         <param field="Mode1" label="Site Name" width="200px" required="true" default="default"/>
         <param field="Mode2" label="MAC Phone Addresses" width="600px" required="true" default="1A:2B:3C:4D:5E:6F,7A:8B:9C:AD:BE:CF"/>
-        <param field="Mode3" label="Notifications" width="75px">
+        <param field="Mode3" label="Interval" width="200px" required="true" default="15"/>
+        <param field="Mode4" label="Notifications" width="75px">
             <options>
                 <option label="True" value="True"/>
                 <option label="False" value="False"  default="true" />
@@ -73,7 +74,7 @@ class BasePlugin:
             Domoticz.Debugging(0)
                 
         self.SetupConnection()
-        Domoticz.Heartbeat(10)
+        Domoticz.Heartbeat(Parameters["Mode3"])
 
     def onStop(self):
         Domoticz.Log("onStop called")
