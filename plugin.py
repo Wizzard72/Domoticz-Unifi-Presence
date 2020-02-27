f015926a43c82fa5a35e233ec05431b8d8678b9f
Wizzard72 <paulslats@gmail.com>
2020-02-27 23:51:51 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -178,15 +178,14 @@ class BasePlugin:
                     'Headers' : { 
                     		'Connection': 'keep-alive', \
 				'Cache-Control': 'max-age=0', \
-				'DNT': 1, \
-				'Upgrade-Insecure-Requests': 1, \
+				#'DNT': 1, \
+				#'Upgrade-Insecure-Requests': 1, \
 				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36', \
 				'Sec-Fetch-Dest': 'document', \
 				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', \
 				'Sec-Fetch-Site': 'none', \
 				'Sec-Fetch-Mode': 'navigate', \
-				'Sec-Fetch-User': '?1', \
-				'Accept-Encoding': 'gzip, deflate, br', \
+			     	'Accept-Encoding': 'gzip, deflate, br', \
 				'Accept-Language': 'en-NL,en;q=0.9,nl-NL;q=0.8,nl;q=0.7,en-US;q=0.6', \
 				'Cookie': 'unifises=IxGj1zWySk63GJdKZ7YWm40ZrUJN33iA; csrf_token=KzPRVxhtrGMQups5gLXeP5kudV2Ds7aN', \
                     		'Host': Parameters["Address"]+":"+Parameters["Port"]
