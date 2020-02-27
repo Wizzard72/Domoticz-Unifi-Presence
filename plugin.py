47f9aa6c238ad534ad64aa30e958ac563b9d74e7
Wizzard72 <paulslats@gmail.com>
2020-02-27 23:48:02 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -178,8 +178,8 @@ class BasePlugin:
                     'Headers' : { 
                     'Connection': 'keep-alive', \
 					'Cache-Control': 'max-age=0', \
-					'DNT': '1', \
-					'Upgrade-Insecure-Requests': '1', \
+					'DNT': 1, \
+					'Upgrade-Insecure-Requests': 1, \
 					'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36', \
 					'Sec-Fetch-Dest': 'document', \
 					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', \
