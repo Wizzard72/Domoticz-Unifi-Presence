32820f33914c40f0f52b06923ce71b0995ac6299
Wizzard72 <paulslats@gmail.com>
2020-02-27 23:37:50 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -179,7 +179,7 @@ class BasePlugin:
                     'Headers' : { 
                         'Connection': 'keep-alive', \
                         'Host': Parameters["Address"]+":"+Parameters["Port"], \
-                        'Cookie': ['unifises='+self.unifises+'; csrf_token='+self.csrftoken]
+                        'Cookie': 'unifises='+self.unifises+'; csrf_token='+self.csrftoken
                     },
                     'Data' : json.dumps(payload)
                    }
