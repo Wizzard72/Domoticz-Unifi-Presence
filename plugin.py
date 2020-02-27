b7e738dc5f8032776ca48442861e4bf2614971d8
Wizzard72 <paulslats@gmail.com>
2020-02-27 23:52:47 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -178,8 +178,8 @@ class BasePlugin:
                     'Headers' : { 
                     		'Connection': 'keep-alive', \
 				'Cache-Control': 'max-age=0', \
-				#'DNT': 1, \
-				#'Upgrade-Insecure-Requests': 1, \
+				'DNT': 1, \
+				'Upgrade-Insecure-Requests': 1, \
 				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36', \
 				'Sec-Fetch-Dest': 'document', \
 				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', \
@@ -190,7 +190,7 @@ class BasePlugin:
 				'Cookie': 'unifises=IxGj1zWySk63GJdKZ7YWm40ZrUJN33iA; csrf_token=KzPRVxhtrGMQups5gLXeP5kudV2Ds7aN', \
                     		'Host': Parameters["Address"]+":"+Parameters["Port"]
                     		#'Cookie': 'unifises='+self.unifises+'; csrf_token='+self.csrftoken
-                    },
+                    }
         Domoticz.Log("RequestDetails sendData = "+str(sendData))
         self.unifiConn.Send(sendData)
     
