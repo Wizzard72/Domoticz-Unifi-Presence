1f15746a90cf7624a85f2f70d8a481a5e72bfd09
Wizzard72 <paulslats@gmail.com>
2020-03-22 11:28:53 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -88,7 +88,7 @@ class BasePlugin:
         if (self.UNIFI_LAN_COUNTER_UNIT not in Devices):
             Domoticz.Device(Name="LAN Counter",  Unit=self.UNIFI_LAN_COUNTER_UNIT, Type=243, Subtype=31).Create()
             UpdateDevice(self.UNIFI_LAN_COUNTER_UNIT, 0, "0.0")
-        for item in Devices;
+        for item in Devices:
             Domoticz.Log(strName+"item in devices = " +item)
         device_mac=Parameters["Mode2"].split(",")
         for device in device_mac:
