7f5bdec32d6b3c22ac80b1c572a1dbc3477b2255
Wizzard72 <paulslats@gmail.com>
2020-03-22 14:25:00 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -285,18 +285,18 @@ class BasePlugin:
                     phone_name = phone_name.strip()
                     mac_id = mac_id.strip().lower()
                     Domoticz.Log(strName+"MAC = " +str(item['mac']+" = "+mac_id)
-                    if str(item['mac']) == str(mac_id):
-                        svalue = "On"
-                        nvalue = 1
-                    else:
-                        svalue = "Off"
-                        nvalue = 0
-                    for item in Devices:
-                        Domoticz.Log(strName+"Device.item = " +Devices[item].Name[8:])
-                        if Devices[item].Name[8:] == "Unifi - "+phone_name:
-                            Domoticz.Log(strName+"Found phone = "+device)
-                            device_unit = Devices[item].ID
-                    UpdateDevice(device_unit, nvalue, svalue)
+                    #if str(item['mac']) == str(mac_id):
+                    #    svalue = "On"
+                    #    nvalue = 1
+                    #else:
+                    #    svalue = "Off"
+                    #    nvalue = 0
+                    #for item in Devices:
+                    #    Domoticz.Log(strName+"Device.item = " +Devices[item].Name[8:])
+                    #    if Devices[item].Name[8:] == "Unifi - "+phone_name:
+                    #        Domoticz.Log(strName+"Found phone = "+device)
+                    #        device_unit = Devices[item].ID
+                    #UpdateDevice(device_unit, nvalue, svalue)
 
    
     def Authenticate(self):
