96de5f2254125c2cda06a656d1f85c18b370269c
Wizzard72 <paulslats@gmail.com>
2020-03-22 11:55:30 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -101,12 +101,13 @@ class BasePlugin:
                 mac_id = mac_id.strip().lower()
                 for item in Devices:
                     if Devices[item].name == phone_name:
+                        Domoticz.Log(strName+"Found phone = "+device)
                         found_phone = True
                 if found_phone == False:
                     Domoticz.Device(Name=phone_name, Unit=new_unit, TypeName="Switch", Used=1).Create()
                     #Domoticz.Status(strName+"Created device for "+phone_name+" with unit id " + str(new_unit))
             except:
-                Domoticz.Error("Invalid phone settings. (" +device+")")
+                Domoticz.Error(strName+"Invalid phone settings. (" +device+")")
 	    
 
         self.SetupConnection()
