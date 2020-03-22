23ca9d229d5f79ad02a35c1c995e9c568eb8da5c
Wizzard72 <paulslats@gmail.com>
2020-03-22 14:39:43 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -293,7 +293,7 @@ class BasePlugin:
                         svalue = "Off"
                         nvalue = 0
                     for item in Devices:
-                        Domoticz.Log(strName+"Device.item = " +Devices[item].Name[8:])
+                        Domoticz.Log(strName+"Device.item = " +Devices[item].Name)
                     #    if Devices[item].Name[8:] == "Unifi - "+phone_name:
                     #        Domoticz.Log(strName+"Found phone = "+device)
                     #        device_unit = Devices[item].ID
