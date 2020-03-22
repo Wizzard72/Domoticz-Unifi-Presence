478af047270c0ef8376a305628f9348ee788e360
Wizzard72 <paulslats@gmail.com>
2020-03-22 11:39:49 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -89,7 +89,7 @@ class BasePlugin:
             Domoticz.Device(Name="LAN Counter",  Unit=self.UNIFI_LAN_COUNTER_UNIT, Type=243, Subtype=31).Create()
             UpdateDevice(self.UNIFI_LAN_COUNTER_UNIT, 0, "0.0")
         for item in Devices:
-            Domoticz.Log(strName+"item in devices = " +Devices[item].DeviceName)
+            Domoticz.Log(strName+"item in devices = " +Devices[item].Name)
         device_mac=Parameters["Mode2"].split(",")
         for device in device_mac:
             device = device.strip()
