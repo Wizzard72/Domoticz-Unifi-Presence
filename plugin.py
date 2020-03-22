e3f2327896a4f2771bf150cd717eb56b19f0a3c9
Wizzard72 <paulslats@gmail.com>
2020-03-22 22:49:28 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -367,7 +367,7 @@ class BasePlugin:
                             Domoticz.Log(strName+"Devices[dv].Name[8:] = "+Devices[dv].Name[8:])
                             Domoticz.Log(strName+"Check "+phone_name+" = "+Devices[dv].Name[8:])
                             device_unit = Devices[dv].Unit
-                            break
+                            continue
                 if found_mac == 1:
                     svalue = "On"
                     nvalue = 1
