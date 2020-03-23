aa84df46a67cdfd2f0d3fc253a5c39cb182039fb
Wizzard72 <paulslats@gmail.com>
2020-03-23 21:49:27 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -378,7 +378,8 @@ class BasePlugin:
                         found_user = phone_name
                         count = 0
                         for dev in self.Matrix:
-                            Domoticz.Log(strName+"DEV = "+dev)
+                            Domoticz.Log(strName+"DEV = "+dev[count][0])
+                            count = count + 1
 				
                     #for dv in Devices:
                         # Find the unit number
