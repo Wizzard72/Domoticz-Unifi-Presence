b5d04d0167c19f411de2d72a531c204047b9d4ff
Wizzard72 <paulslats@gmail.com>
2020-03-27 13:28:36 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -227,7 +227,7 @@ class BasePlugin:
         #for extra in device_extra:
         #    self.total_devices_count = self.total_devices_count + 1
         #extra_devices = 1 # Override device
-        self.total_devices_count = self.total_devices_count + extra_devices
+        #self.total_devices_count = self.total_devices_count + extra_devices
         Domoticz.Log(strName+"Count = "+str(self.total_devices_count))
         w, h = 5, self.total_devices_count;
         self.Matrix = [[0 for x in range(w)] for y in range(h)] 
