908595e8328f24f6b63a70933a272b2326b20174
Wizzard72 <paulslats@gmail.com>
2020-03-27 15:42:25 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -590,15 +590,18 @@ class BasePlugin:
                     else:
                         UpdateDevice(self.Matrix[x][2], nvalue, svalue)
                         self.Matrix[x][3] = svalue
-            #else:
-                #if self.Matrix[x][5] == "NO":
-            #    svalue = "Off"
-            #    nvalue = 0
-            #    if self.Matrix[x][0] == "OverRide":
-            #        UpdateDevice(self.Matrix[x][2], nvalue, "0")
-            #    else:
-            #        UpdateDevice(self.Matrix[x][2], nvalue, svalue)
-            #        self.Matrix[x][3] = svalue
+            elif self.Matrix[x][4] == "Yes" and self.Matrix[x][5] == "NO":
+                Domoticz.Log(strName+"Nothing to do")
+            elif self.Matrix[x][4] == "No" and self.Matrix[x][5] == "NO":
+                Domoticz.Log(strName+"Nothing to do")
+            else:
+                svalue = "Off"
+                nvalue = 0
+                if self.Matrix[x][0] == "OverRide":
+                    UpdateDevice(self.Matrix[x][2], nvalue, "0")
+                else:
+                    UpdateDevice(self.Matrix[x][2], nvalue, svalue)
+                    self.Matrix[x][3] = svalue
             
         
         count = 0
